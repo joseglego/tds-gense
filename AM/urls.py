3fdad950a86f0defca34c6909adfea81266c12d1
Jose Lezama Gonzalez <mistwalker.jl@gmail.com>
2013-05-25 19:26:41 -0430

    Corregidos TODOS los errores despues del commit, habia faltado urls

--- AM/urls.py
+++ AM/urls.py
@@ -10,26 +10,18 @@ urlpatterns = patterns('',
     url('^$','app_usuario.views.sesion_iniciar'),
     url('^sesion/cerrar$','app_usuario.views.sesion_cerrar'),
     url('^usuario/solicitar$','app_usuario.views.usuario_solicitar'),
-<<<<<<< HEAD
     url('^usuario/clave$','app_usuario.views.clave_cambiar'),
     url('^usuario/restablecer$','app_usuario.views.clave_restablecer'),
-                      
-=======
-	url(r'^',include('app_atencion.urls')),
->>>>>>> ea3ac9b4fdf9693b806022f20edbad36fa1cdb8a
+    url(r'^',include('app_atencion.urls')),
 
     # Emergencias
     url('^emergencia/agregar$','app_emergencia.views.emergencia_agregar'),
     url('^emergencia/listar$','app_emergencia.views.emergencia_listar'),
     url('^emergencia/(?P<idE>\d+)/t(?P<vTriage>\d+)$','app_emergencia.views.emergencia_aplicarTriage'),
-<<<<<<< HEAD
-    url('^emergencia/(?P<idE>\d+)/triage/calcular$','app_emergencia.views.emergencia_calcularTriage'),        
-=======
-#    url('^emergencia/(?P<idE>\d+)/triage/calcular$','app_emergencia.views.emergencia_calcularTriage'),   
+    url('^emergencia/(?P<idE>\d+)/triage/calcular$','app_emergencia.views.emergencia_calcularTriage'),   
 
-		# Paciente
-		url('^paciente/listarPacientes$','app_paciente.views.paciente_listarPacientes'),    
->>>>>>> ea3ac9b4fdf9693b806022f20edbad36fa1cdb8a
+    # Paciente
+    url('^paciente/listarPacientes$','app_paciente.views.paciente_listarPacientes'),    
                        
     ## COSAS DJANGISTICAS
     # Admin
