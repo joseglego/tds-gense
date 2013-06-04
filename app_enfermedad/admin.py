bbc3144d55b4f12242416979349845a2bc5da983
Katty <kate.sare.89@gmail.com>
2013-06-03 21:08:12 -0430

    tablas enfermedad en admin

---
+++ app_enfermedad/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from app_enfermedad.models import *
+admin.site.register(Causa)
+admin.site.register(Problema)
+admin.site.register(ParteCuerpo)
+admin.site.register(Presente)
+admin.site.register(OrigenProblema)
