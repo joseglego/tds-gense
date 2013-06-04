05b57e3fc33c49f9bac39d39f25fbe06e4ff5a6e
Katty <kate.sare.89@gmail.com>
2013-06-03 20:53:34 -0430

    urls para atencion

---
+++ app_emergencia/urls.py
@@ -0,0 +1,35 @@
+from django.conf.urls import patterns, url, include
+
+from app_emergencia import views
+
+urlpatterns = patterns('app_emergencia.views',
+
+    # Funciones de acceso a subfases de atencion
+    
+    # Acceso por default a antecedentes:
+    url('^emergencia/atencion/(?P<id_emergencia>.*)$','emergencia_atencion'), 
+    
+    # Ingreso a antecedentes
+    url('^emergencia/antecedentes/(?P<id_emergencia>.*)$','emergencia_antecedentes'),
+    
+    # Ingreso a Examen fisico
+    url('^emergencia/enfermedad/(?P<id_emergencia>.*)$','emergencia_enfermedad'),
+    
+    # Ingreso a Indicaciones Terapeuticas
+    url('^emergencia/indicacionesT/(?P<id_emergencia>.*)$','emergencia_indicacionesT'),
+
+    # Ingreso a Indicaciones Diagnosticas
+    url('^emergencia/indicacionesD/(?P<id_emergencia>.*)$','emergencia_indicacionesD'),
+
+    # Ingreso a Diagnostico Definitivo
+    url('^emergencia/diagnostico/(?P<id_emergencia>.*)$','emergencia_diagnostico'),
+
+
+    # Botones genericos de atencion:
+    # Descargar historia medica 
+    url('^emergencia/descarga/(?P<id_emergencia>.*)$','emergencia_descarga'),
+
+    # Funciones atencion Enfermedad Actual:
+    # Agregar narrativa de enfermedad
+    url('^emergencia/agregarCom$','emergencia_comentario'),				    
+)
