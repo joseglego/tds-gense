1844efd1368a0a6ed42f2741289f1642466b5453
Isa Jilo <isajilo7@gmail.com>
2013-08-02 20:20:46 -0430

    Arreglado formato de fecha

--- app_emergencia/forms.py
+++ app_emergencia/forms.py
@@ -12,7 +12,7 @@ ATENCION = (
 )
 
 class AgregarEmergenciaForm(forms.Form):
-    ingreso          = forms.DateTimeField(label="Hora y Fecha de Ingreso",widget=forms.TextInput(attrs={'placeholder':'dd/MM/aaaa hh:mm:ss','data-format':'dd-MM-yyyy hh:mm:ss'}))
+    ingreso          = forms.DateTimeField(label="Hora y Fecha de Ingreso",widget=forms.TextInput(attrs={'placeholder':'dd/MM/aaaa hh:mm:ss','data-format':'dd/MM/yyyy hh:mm:ss'}))
     cedula           = forms.CharField(label="Número de Cédula",max_length=9)
     nombres          = forms.CharField(label="Nombres", max_length=64)
     apellidos        = forms.CharField(label="Apellidos", max_length=64)
@@ -41,7 +41,7 @@ class BuscarEmergenciaForm(forms.Form):
 
 
 class calcularTriageForm(forms.Form):
-    fecha         = forms.DateTimeField(label="Fecha y hora a la que se realiza la Evaluación",widget=forms.TextInput(attrs={'placeholder':'dd/MM/aaaa hh:mm:ss','data-format':'dd-MM-yyyy hh:mm:ss'}))
+    fecha         = forms.DateTimeField(label="Fecha y hora a la que se realiza la Evaluación",widget=forms.TextInput(attrs={'placeholder':'dd/MM/aaaa hh:mm:ss','data-format':'dd/MM/yyyy hh:mm:ss'}))
     motivo        = forms.ModelChoiceField(label="Motivo de Ingreso",required=False,queryset=Motivo.objects.exclude(nombre__startswith=" "))
     ingreso       = forms.CharField(label="Tipo de Ingreso",required=False,max_length=1,widget=forms.Select(choices=ICAUSA))
     
